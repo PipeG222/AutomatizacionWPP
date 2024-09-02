563f1ccecce1868d71d4e9acbde71039bc0d0dea
jacoEafit <jmontesca@eafit.edu.co>
2024-09-02 11:02:45 -0500

    estructura urls superadmin

--- superadmin/urls.py
+++ superadmin/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.superadmin , name="superadmin"),
+    path('', views.superadmin , name="superadmin"), 
+    path('empresas_superadmin/', views.empresas_superadmin , name="empresas_superadmin"),
+    path('negocios_superadmin/', views.negocios_superadmin , name="negocios_superadmin"),
+    path('servicios_superadmin/', views.servicios_superadmin , name="servicios_superadmin"),
+    path('usuarios_superadmin/', views.usuarios_superadmin , name="usuarios_superadmin"),
+    path('clientes_superadmin/', views.clientes_superadmin , name="clientes_superadmin"),
+    path('citas_superadmin/', views.citas_superadmin , name="citas_superadmin"),
+    path('notificaciones_superadmin/', views.notificaciones_superadmin , name="notificaciones_superadmin"),
 ]
--- superadmin/views.py
+++ superadmin/views.py
@@ -4,3 +4,25 @@ from django.shortcuts import render
 def superadmin(request):
     
     return render(request,'superadmin.html')
+
+
+def empresas_superadmin(request):
+    pass
+
+def negocios_superadmin(request):
+    pass
+
+def servicios_superadmin(request):
+    pass
+
+def usuarios_superadmin(request):
+    pass
+
+def clientes_superadmin(request):
+    pass
+
+def citas_superadmin(request):
+    pass
+
+def notificaciones_superadmin(request):
+    pass
